c58da1dbbecdaaef429fbb86f52ef2b909335987
Matheus Guerra <matheusdmguerra@gmail.com>
2023-03-07 23:59:38 -0300

    parte de exibição dos produtos quase pronta

--- loja-Ninja/src/app/app-routing.module.ts
+++ loja-Ninja/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
-
+//Routa já definida na criação do módulo "produtos"
+const routes: Routes = [{ path: 'produtos', loadChildren: () => import('./produtos/produtos.module').then(m => m.ProdutosModule) },
+{path: "", redirectTo: "produtos", pathMatch: "full"}];
+//Após deixar a classe de rotas pronta, adicionar no app.component.html
 @NgModule({
+  declarations:[],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
